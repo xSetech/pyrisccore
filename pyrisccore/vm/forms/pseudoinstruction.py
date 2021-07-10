a19fb8f2b64a44ae8c977dd6b2c23c9e0bf11be0
Seth Junot <xsetech@gmail.com>
2021-07-10 09:18:44 -0700

    Constant fields in a pseudo-instruction are optional

--- pyrisccore/vm/forms/pseudoinstruction.py
+++ pyrisccore/vm/forms/pseudoinstruction.py
@@ -1,7 +1,7 @@
 """ A "pseudo-instruction" is a mnemonic corresponding to the constant value of specific fields
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Dict, Tuple, Union
 
 from pyrisccore.vm.forms.field import Field
@@ -16,7 +16,7 @@ class PseudoInstruction:
 
     mnemonic: str               # e.g. "ADDI"
     operation: Operation        # e.g. OP-IMM, gives the instruction format & opcode
-    constants: Dict[Union[Field, str], int]   # e.g. "funct3": 0b000 for "ADDI"
+    constants: Dict[Union[Field, str], int] = field(default_factory=dict)  # e.g. "funct3": 0b000 for "ADDI"
     subfields: Tuple[Format] = tuple()   # e.g. "shamt", derived from imm
 
 
